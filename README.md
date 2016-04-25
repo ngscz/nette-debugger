70b2d475b3aafb952b45374bbb43ec3930d0c326
Svatopluk Hastík <svatopluk.hastik@ngs.cz>
2016-04-25 13:54:26 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
-# nette-debugger
+# NetteDebugger #
+
+Easy option, how to **manage Nette framework exceptions** from log dir.
+
+Also add method to **clear cache and log** by clicking on button in browser.
+
+## Instalation ##
+
+Use composer
+
+Via command line: `composer require ngscz/nette-debugger`.
+
+## Usage ##
+
+Create PHP file `debugger.php` in your web root directory.
+
+and add this code:
+
+```php
+<?php 
+
+require dirname(__FILE__) . '/../vendor/autoload.php';
+
+use ngscz\NetteDebugger;
+
+$debugger = new NetteDebugger\Debugger();
+$debugger->run();
+```
+
+
+Open `/debugger.php` in your favourite browser.
---
+++ src/Debugger.php
@@ -0,0 +1,213 @@
+<?php
+namespace ngscz\NetteDebugger;
+
+use DOMDocument;
+
+//TODO vypsat datum, kdy výjimka nastala
+//TODO načítat externě bootstrap CSS
+
+class Debugger {
+	
+	private $html = array();
+	
+	public function run() {
+		
+		$action = (isset($_GET['action']))?$_GET['action']:false;
+		
+		switch ($action) {
+			case 'show':
+				$this->createExceptionPage($_GET['file']);
+			break;
+			
+			case 'clearCache':
+				$this->clearCache();
+			break;
+
+			case 'clearLog':
+				$this->clearLog();
+			break;
+		
+			case 'delete':
+				$this->deleteExceptionFile($_GET['file']);
+			break;
+			
+			default:
+				$this->createTopButtons();
+				$this->createLogTable();		
+			break;
+		}
+				
+		
+		$this->createCssStyles();				
+				
+		$this->renderHTML();
+	}
+	
+	private function renderHTML() {
+		echo implode('',$this->html);
+	}
+	
+	private function deleteExceptionFile($file) {
+		$files = $this->getLogFiles();
+		if (isset($files[$file])) {
+			unlink($files[$file]);
+			$this->redirectHome();
+		} else {
+			$this->html[] = 'Exception not found.';
+		}				
+	}
+	
+	private function clearCache() {
+		$this->clearDir($this->getCacheDir());
+		$this->redirectHome();
+	}
+	
+	private function clearLog() {
+		foreach (scandir($this->getLogDir()) as $file => $absPath) {
+			if (in_array($file, array('.', '..'))) continue;
+			if (strstr($file, '.html') || strstr($file, '.log')) {
+				unlink($absPath);
+			}
+		}
+		$this->redirectHome();
+	}
+	
+	private function createExceptionPage($file) {
+		$files = $this->getLogFiles();
+		if (isset($files[$file])) {
+			$this->html[] = file_get_contents($files[$file]);
+		} else {
+			$this->html[] = 'Exception not found.';
+		}
+	}
+	
+	private function redirectHome() {		
+		header(sprintf('Location: %s', $_SERVER['SCRIPT_NAME']));
+		exit();
+	}
+	
+	private function createTopButtons() {
+		$this->html[] = $this->renderContentTag('a', 'Clear cache', array(
+			'href' => '?action=clearCache'
+		));
+		$this->html[] = $this->renderContentTag('a', 'Clear log', array(
+			'href' => '?action=clearLog'
+		));		
+	}
+	
+	private function createLogTable() {
+		$trs = array();
+		foreach ($this->getLogFiles() as $file => $absPath) {
+			$tds = array();
+			
+			
+			$content = file_get_contents($absPath);
+			$dom = new DOMDocument('1.0', 'UTF-8');
+			@$dom->loadHTML($content);        						
+			foreach ($dom->getElementsByTagName('title') as $elm) {
+					$title = $elm->nodeValue;
+					break;
+			}			
+			
+			$tds[] = $this->renderContentTag('td', $title);
+			
+			//options
+			$showLink = $this->renderContentTag('a', 'Show', array(
+				'target' => '_blank',
+				'href'	 => '?action=show&file=' . $file
+			));
+			$deleteLink = $this->renderContentTag('a', 'Delete', array(				
+				'href'	 => '?action=delete&file=' . $file
+			));
+			
+			$tds[] = $this->renderContentTag('td', $showLink . $deleteLink);
+			
+			$trs[] = $this->renderContentTag('tr', implode('',$tds));
+		}
+		
+		$table = $this->renderContentTag('table', implode('',$trs));
+		
+		$this->html[] = $table;		
+	}
+	
+	private function createCssStyles() {
+		$styles = $this->renderContentTag('style');
+		
+		$this->html[] = $styles;
+	}
+	
+	private function renderContentTag($name, $content = '', $attributes = array()) {
+		
+		$htmlAttributes = array();
+		foreach ($attributes as $aname => $avalue) {
+			$htmlAttributes[] = sprintf('%s="%s"', $aname, $avalue);
+		}
+		
+		return 
+			sprintf('<%s %s>%s</%s>',
+				$name,
+				implode(' ', $htmlAttributes),
+				$content,
+				$name
+			);
+	}
+	
+	public function getLogFiles() {
+		$logDir = $this->getLogDir();		
+		$files = array();
+		
+		foreach (scandir($logDir) as $file) {
+			if (in_array($file, array('.','..'))) continue;
+			if (!strstr($file, '.html')) continue;
+			
+			$absPath = $logDir . DIRECTORY_SEPARATOR . $file;
+			
+			$files[$file] = $absPath;						
+		}		
+		
+		return $files;
+	}
+	
+	public function getCacheDir() {
+		return 
+		dirname(__FILE__) . DIRECTORY_SEPARATOR .
+		'..' . DIRECTORY_SEPARATOR .
+		'..' . DIRECTORY_SEPARATOR . 
+		'..' . DIRECTORY_SEPARATOR . 
+		'..' . DIRECTORY_SEPARATOR . 
+		'temp' . DIRECTORY_SEPARATOR .
+		'cache'
+		;
+		
+	}
+	
+	public function getLogDir() {
+		return 
+		dirname(__FILE__) . DIRECTORY_SEPARATOR .
+		'..' . DIRECTORY_SEPARATOR .
+		'..' . DIRECTORY_SEPARATOR . 
+		'..' . DIRECTORY_SEPARATOR . 
+		'..' . DIRECTORY_SEPARATOR . 
+		'log';
+	}
+	
+	public function clearDir($dir, $deleteDir = false) {
+			if (is_dir($dir)) {
+					$objects = scandir($dir);
+					foreach ($objects as $object) {
+							if ($object != "." && $object != "..") {
+									if (filetype($dir . "/" . $object) == "dir") {
+										$this->clearDir($dir . "/" . $object, true);
+									} else {
+										@unlink($dir . "/" . $object);
+									}
+							}
+					}
+					reset($objects);
+					if ($deleteDir) {
+						@rmdir($dir);
+					}	
+			}
+	}
+	
+}
